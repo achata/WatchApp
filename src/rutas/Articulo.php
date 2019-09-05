abe4290435c32f94b9883c7977a36c381c29b00a
Achata <a.achata.j@gmail.com>
2019-09-05 04:29:53 -0500

    add new end point qr

--- src/rutas/Articulo.php
+++ src/rutas/Articulo.php
@@ -4,7 +4,6 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 //$app = AppFactory::create();
-//GET Estudiante
 
 $app->get('/api/articulo/estudiante/{id}',function(Request $request, Response $response){
     $id_estudiante = $request->getAttribute('id');
@@ -55,7 +54,7 @@ $app->post('/api/articulo',function(Request $request, Response $response){
         $conexion = Conexion::openConnect();
         $resultado = $conexion->prepare($usp);
         
-        $resultado->bind_Param('ssssssss',
+        $resultado->bind_Param('isssssss',
             $idEstudiante,
             $nombre,
             $marca,
@@ -78,6 +77,30 @@ $app->post('/api/articulo',function(Request $request, Response $response){
         echo json_encode($e);
     }
 
+    return $response;
+});
+
+$app->post('/api/articulo/qr', function(Request $request, Response $response){
+    
+    $qr = $request->getParam("qr");
+    $array = array();
+    $usp = "CALL USP_BUSCAR_ARTICULO(?)";
+
+    try {
+        $conexion = Conexion::openConnect();
+        $resultado = $conexion->prepare($usp);
+        $resultado->bind_param('s',$qr);
+        $resultado->execute();
+        $rs = $resultado->get_result();
+        //$resultado->bind_result($c1,$c2,$c3,$c4,$c5,$c6,$c7,$c8,$c9);
+        /*while($row = $rs->fetch_array(MYSQLI_ASSOC)){
+            $array[] = $row;
+        }*/
+        $array[] = $rs->fetch_array(MYSQLI_ASSOC);
+        echo json_encode($array);
+    } catch (mysqli_sql_exception $e) {
+        echo json_encode($e);
+    }
     return $response;
 });
 
