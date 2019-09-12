63524d11e4ad9234150bd01652c8bfa77cf5dd43
Achata <a.achata.j@gmail.com>
2019-09-12 05:43:50 -0500

    add methos get student

--- src/config/Conexion.php
+++ src/config/Conexion.php
@@ -1,13 +1,10 @@
 <?php 
 
 class Conexion{
-    private $host = 'localhost';
-    private $user = 'root';
-    private $password = '';
-    private $database = 'watchapp';
+    
 
     public static function openConnect() {
-        $conexion = new mysqli('localhost','root','','watchapp');
+        $conexion = new mysqli('85.10.205.173:3306','watchapp','','watchapp');
         //$conexion = new mysqli($this->$host,$this->$user,$this->$password,$this->$database);
         if($conexion->connection_errno){
             echo "ERROR CONEXION";
--- src/rutas/Estudiante.php
+++ src/rutas/Estudiante.php
@@ -3,13 +3,22 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-$app->post('/api/estudiante',function(Request $request, Response $response){
+$app->post('/api/estudiante/{id}',function(Request $request, Response $response){
     
+    $id = $request->getAttribute('id');
+    $usp = "USP_SEL_ESTUDIANTE_ID($id)";
+    $array = array();
     try {
-
-        
+        $conexion = Conexion::openConnect();
+        $resultado = $conexion->query($usp);
+        if($resultado->num_rows > 0){
+            while($row = $resultado->fetch_array(MYSQLI_ASSOC)){
+                $array[] = $row;
+            }
+            echo json_encode($array);
+        }   
     } catch (mysqli_sql_exception $e) {
-        
+        echo $e;
     }
 
     return $response;
